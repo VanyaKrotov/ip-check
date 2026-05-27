dc91063edfc56b2ade09aef8d20590690e2ee730
Ivan Krotov <vanyakrotov3059@gmail.com>
2026-05-27 08:12:49 +0300

    feat: add favicon & improve runner

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
+  server: {
+    proxy: {},
+    host: true,
+  },
 });
